084c3aecb145547fe680f88bad11e9f4a4e2f8e7
James <navikasjokubas@gmail.com>
2025-11-01 21:47:25 +0200

    second and a half commit

--- U1/U1.cpp
+++ U1/U1.cpp
@@ -10,11 +10,22 @@ void uzduotiesKodasCia()
 {
     int x;
     cin >> x;
-    if (){
-        cout << "dalinasi is 5";
-    }
-    if (){
-        cout << "dalinasi is 2";
-    }
+    if (x % 5 == 0)
+    {
+        cout << "dalinasi is 5\n";
 
+    }
+    else {cout << "nesidalina is 5\n";}
+    if (x % 2 == 0)
+    {
+        cout << "dalinasi is 2\n";
+    } else {cout << "Nesidalina is 2\n";}
+    if (x % 2 != 0 && x % 5 != 0;){cout << "nesidalina nei is 5 nei is 2\n";}
+
+}
+
+int main()
+{
+    uzduotiesKodasCia();
+    return 0;
 }
